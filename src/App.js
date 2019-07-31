18ef7806aa26d567aed749a391dff2e7ab5664a1
Ariel Lopez <ariel.lopezf@gmail.com>
2019-07-31 09:57:02 +0800

    first commit f

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Counter from './counter';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      mount: true,
+      ignoreProp: 0,
+      seed: 40
+    }
+
+    this.mountCounter = () => this.setState({mount: true})
+    this.unMountCounter = () => this.setState({mount: false})
+    this.ignoreProp  = () => this.setState({ignoreProp: Math.random()})
+    this.seedGenerator  = () => this.setState({seed: Number.parseInt(Math.random() * 100) })
+  }
+
+  render (){
+    return <div>
+      <button className="btn-default" onClick={this.mountCounter} disabled={this.state.mount}>Mount Counter</button>
+      <button className="btn-default" onClick={this.unMountCounter} disabled={!this.state.mount}>Unmount Counter</button>
+      <button className="btn-default" onClick={this.ignoreProp} >Ignore Prop</button>
+      <button className="btn-default" onClick={this.seedGenerator} >Generate Seed</button>
+      <div className="App">
+        {this.state.mount ?
+            <Counter ignoreProp={ this.state.ignoreProp }
+                     seed={ this.state.seed }
+            /> :
+            null}
+      </div>
     </div>
-  );
+  }
+
 }
 
 export default App;
---
+++ src/counter.js
@@ -0,0 +1,88 @@
+import React from 'react';
+const ErrorComponent  =  () => <div>{props.ignore}</div>;
+
+class Counter extends React.Component {
+  constructor(props){
+    console.log('Constructor - First Page Load')
+    super(props);
+    this.state = {
+      counter: 0,
+      seed: 0
+    };
+
+    this.increment = () => this.setState({counter: this.state.counter + 1})
+    this.decrement = () => this.setState({counter: this.state.counter - 1})
+
+  }
+
+  static getDerivedStateFromProps (props, state){
+    // use to copy props to and assign to this(current component) state
+    console.log( props.seed )
+    console.log( state.seed )
+    if(props.seed && state.seed !== props.seed){
+      return {
+        seed: props.seed,
+        counter: props.seed
+      }
+    }
+    return null;
+  }
+
+  componentDidMount (){
+    console.log("Component Did Mount - First Page Load");
+    console.log("--------------------------------------------------------");
+  }
+
+  shouldComponentUpdate (nextProps, nextState){
+
+    console.log("should component update - trigger on parent class  ( use to check if this components need to rerender )");
+    if(nextProps.ignoreProp &&
+      this.props.ignoreProp !== nextProps.ignoreProp){
+      console.log("should component update - false")
+      return false
+    }
+    console.log("should component update - true")
+    console.log("---------------------")
+    return true
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState){
+    console.log("get SnapShot before update")
+    return null
+  }
+
+  render () {
+
+    console.log("Render - Page Load or event click")
+    if (this.state.error){
+      return <div>We Encounter an error { this.state.error.message }<div>
+    }
+
+    return <div>
+      <button className="btn-default" onClick={this.increment}>Incremenet</button>
+      <button className="btn-default" onClick={this.decrement}>Decremenet</button>
+      <div className="Counter">
+        <h1> Counter: {this.state.counter} </h1>
+      </div>
+      <ErrorComponent/>
+    </div>
+  }
+
+  componentDidUpdate (prevProps, prevState, snapshot){
+    console.log("Component Did Update - Trigger on Event Click")
+    console.log('---------------------')
+  }
+
+  componentWillUnmount () {
+    console.log("Component Will UnMount trigger on parent if not display/render")
+    console.log("--------------------------------------")
+  }
+
+  componentDidCatch (error, info){
+    console.log('Component did Catch')
+    this.setState({error, info})
+  }
+
+}
+
+export default Counter;
